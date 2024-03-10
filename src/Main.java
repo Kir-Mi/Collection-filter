8d479bf736a0e5724fc82412e5575d311a891a5a
kirill <you_can_more@mail.ru>
2024-03-10 21:07:45 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[] array = {1, 2, 3, 4, 5};
+        Filter<Integer> filter = new FilterImpl<Integer>();
+        Integer[] ints = filter(array, filter);
+    }
+
+    public static <T> T[] filter(T[] array, Filter<T> filter) {
+        return Arrays.stream(array)
+                .map(filter::apply)
+                .toArray(size -> Arrays.copyOf(array, size));
+    }
+
+    interface Filter<T> {
+        Object apply(T o);
+    }
+
+    static class FilterImpl<T> implements Filter<T> {
+        @Override
+        public Object apply(Object o) {
+            System.out.println(o.toString());
+            return o;
+        }
     }
 }
